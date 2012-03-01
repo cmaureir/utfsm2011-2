2bade924e15476756be667dc939f9a658b35d73c
Cristian Maureira Fredes <cmaureir@csrg.inf.utfsm.cl>
2012-03-01 19:10:06 -0300

    Adding plot of the bodies CG Signed-off-by: Cristian Maureira Fredes <cmaureir@csrg.inf.utfsm.cl>

--- Grafica-cmaureir/bodies.cpp
+++ Grafica-cmaureir/bodies.cpp
@@ -2,10 +2,12 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 GLsizei swh = 400;
 GLsizei sww = 400;
+GLsizei swz = 400;
 GLfloat xgs = 1, ygs = 1,xgp = 0, ygp = 0;
 GLfloat xs = 1, ys = 1;
 
@@ -14,31 +16,104 @@ struct body {
     float x,y,z;
 };
 
-void read_info(){
+// Information
+int n;
+int it;
+float x_max, y_max, z_max;
+float x_min, y_min, z_min;
+vector<body> bodies;
+vector<body> positions;
+
+void read_info(string path){
+    ifstream info(path.c_str());
+    string line;
+    int l = 0;
+    while (info.good())
+    {
+        if (l == 0)
+        {
+            getline(info,line);
+            n = atoi(line.c_str());
+        }
+        else if (l == 1)
+        {
+            getline(info,line);
+            it = atoi(line.c_str());
+        }
+        else if (l == 2)
+        {
+            getline(info, line, ' ');
+            getline(info, line, ' ');
+            x_min = atof(line.c_str());
+            getline(info, line, ' ');
+            x_max = atof(line.c_str());
+        }
+        else if (l == 3)
+        {
+            getline(info, line, ' ');
+            getline(info, line, ' ');
+            y_min = atof(line.c_str());
+            getline(info, line, ' ');
+            y_max = atof(line.c_str());
+        }
+        else if (l == 4)
+        {
+            getline(info, line, ' ');
+            getline(info, line, ' ');
+            z_min = atof(line.c_str());
+            getline(info, line, ' ');
+            z_max = atof(line.c_str());
+        }
+        l++;
+    }
 
 }
 
-void read_input(){
-
+void read_input(string path){
+    ifstream info(path.c_str());
+    string line;
+    body tmp;
+    while (info.good())
+    {
+        getline(info,line,' ');
+        tmp.id = atoi(line.c_str());
+        getline(info,line,' ');
+        tmp.x = atof(line.c_str());
+        getline(info,line,' ');
+        tmp.y = atof(line.c_str());
+        getline(info,line,' ');
+        tmp.z = atof(line.c_str());
+        bodies.push_back(tmp);
+    }
 }
 
-void read_results(){
+void read_results(string path){
+    ifstream info(path.c_str());
+    string line;
+    body tmp;
+    while (info.good())
+    {
+        getline(info,line,' ');
+        tmp.id = atoi(line.c_str());
+        getline(info,line,' ');
+        tmp.x = atof(line.c_str());
+        getline(info,line,' ');
+        tmp.y = atof(line.c_str());
+        getline(info,line,' ');
+        tmp.z = atof(line.c_str());
+        bodies.push_back(tmp);
+    }
 
 }
-
-float xx[] = {100,100,200};
-float yy[] = {100,200,200};
-float zz[] = {0,0,0};
 
 void DisplayBodies()
 {
 
     glClear(GL_COLOR_BUFFER_BIT);
     glTranslatef(0,0,0);
-    //glScalef(5.0,5.0, 1.0);
     glBegin(GL_POINTS);
-    for (int i = 0; i < 3; i++) {
-        glVertex3f(xx[i],yy[i],zz[i]);
+    for (int i = 0; i < n; i++) {
+        glVertex3f((sww/2.0)+bodies[i].x*20,(swh/2.0)+bodies[i].y*20,bodies[i].z);
     }
     glEnd();
 
@@ -56,19 +131,15 @@ void displays(void)
     glutSwapBuffers();
 }
 
-void refreshDisplay(void)
+void refresh(void)
 {
     glutSetWindow(staticS);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glutPostRedisplay();
+
 }
 
-void myinit(void)
-{
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glColor3f(1.0, 1.0, 1.0);
-}
 
 void myReshape(int w, int h)
 {
@@ -77,36 +148,25 @@ void myReshape(int w, int h)
     glMatrixMode(GL_MODELVIEW);
 
 }
-
-void update(int value)
-{
-    xx[0] += 1;
-    xx[1] += 1;
-    xx[2] += 1;
-    glutPostRedisplay();
-    glutTimerFunc(25, update, 0);
-
-}
 void keys(unsigned char key, int x, int y)
 {
     switch(key)
     {
         case 'x':
-            xx[0] += 20;
-            xx[1] += 20;
-            xx[2] += 20;
             break;
         case 'y':
-            yy[0] += 20;
-            yy[1] += 20;
-            yy[2] += 20;
             break;
         case 'z':
-            zz[0] += 1;
-            zz[1] += 1;
-            zz[2] += 1;
             break;
     }
+}
+
+void init(void)
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glColor3f(1.0, 1.0, 1.0);
+    read_info("input/256info");
+    read_input("input/256init");
 }
 
 int main(int argc, char** argv)
@@ -118,11 +178,10 @@ int main(int argc, char** argv)
     glutInitWindowPosition (0, 0); // window position on screen
     staticS=glutCreateWindow("Bodies");
 
-    myinit();
+    init();
     glutDisplayFunc(displays);
     glutReshapeFunc(myReshape);
-    glutTimerFunc(25, update, 0);
     glutKeyboardFunc(keys);
-    glutIdleFunc(refreshDisplay);
+    glutIdleFunc(refresh);
     glutMainLoop();
 }
